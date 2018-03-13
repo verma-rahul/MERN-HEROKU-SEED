63f764c6e375687fe8d1aa0f7b6f4c4e449f62bd
Rahul Verma <rahulverma@ccs.neu.edu>
2018-03-13 15:48:53 -0400

    changes to UI with basic layout

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +10,11 @@ class App extends Component {
   }
 
   componentDidMount() {
+  fetchMessage()
+  }
+
+  fetchMessage()
+  {
     fetch('/api')
       .then(response => {
         if (!response.ok) {
@@ -34,18 +37,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div>
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          {'This is '}
-          <a href="https://github.com/mars/heroku-cra-node">
-            {'create-react-app with a custom Node/Express server'}
-          </a><br/>
-        </p>
-        <p className="App-intro">
+        <p>
           {this.state.fetching
             ? 'Fetching message from API'
             : this.state.message}
